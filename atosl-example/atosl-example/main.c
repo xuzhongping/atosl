76ca4f48d525952d99b96c0fbb63fc8642b587f2
徐仲平 <zhongping.xu@ushow.media>
2021-01-14 20:01:50 +0800

    update

---
+++ atosl-example/atosl-example/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  atosl-example
+//
+//  Created by xuzhongping on 2021/1/14.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
